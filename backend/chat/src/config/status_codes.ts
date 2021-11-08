be45aea2b7376d71bc02414f3d047f67a2f05e8d
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-11-08 20:15:46 +0100

    better pipeline and some fixes

--- backend/chat/src/config/status_codes.ts
+++ backend/chat/src/config/status_codes.ts
@@ -3,11 +3,12 @@ export default new class status_codes {
   SESSION_INITIALIZED = [ 100, 'Session successfully initialized' ];
 
   // Failure
-  SESSION_EXISTS = [ 1, 'Session already exists' ];
+  SESSION_ALREADY_INITIALIZED = [ 1, 'Session already initialized' ];
   USERNAME_IN_USE = [ 2, 'Username is already in use' ];
   TARGET_NOT_FOUND = [ 3, 'Target user could not be found' ];
   TARGET_SELF = [ 4, 'You cannot send messages to yourself' ];
   ALREADY_IN_PIPELINE = [ 5, 'You are already in a chat room' ];
   TARGET_ALREADY_IN_PIPELINE = [ 6, 'Target user is already in a chat room' ];
   NOT_IN_PIPELINE = [ 7, 'You are not in a chat room' ];
+  PIPELINE_ENDED = [ 8, 'Chat room has ended' ];
 }
--- backend/chat/src/server.ts
+++ backend/chat/src/server.ts
@@ -3,86 +3,88 @@ import status_codes from '@config/status_codes';
 
 const server = new io.Server();
 
-const clients: { [key: string]: io.Socket } = {};
-const pipeline: Array<io.Socket[]> = [];
+const clients: { [username: string]: io.Socket } = {};
+const pipeline: Array<{ id: string, sockets: Array<io.Socket> }> = new Array<{ id: string, sockets: Array<io.Socket> }>();
+
+// TODO: socket.handshake.query.username
 
 server.on('connection', (socket) => {
   socket.on('initialize-session', (data) => {
-    const username = data.toString();
+    const username: string = data.toString();
 
-    if (Object.values(clients).find((client) => client.id === socket.id)) {
-      socket.emit('chat-error', status_codes.SESSION_EXISTS);
+    if (clients[username]) {
+      socket.emit('initialize-session', status_codes.USERNAME_IN_USE);
       return;
     }
 
-    if (clients[username]) {
-      socket.emit('chat-error', status_codes.USERNAME_IN_USE);
+    if (Object.values(clients).includes(socket)) {
+      socket.emit('initialize-session', status_codes.SESSION_ALREADY_INITIALIZED);
       return;
     }
 
     clients[username] = socket;
+    
     console.log('New session: ' + username);
 
     socket.emit('chat-error', status_codes.SESSION_INITIALIZED);
   });
 
-  socket.on('start-chat', (data) => {
-    const target = data.toString();
-
-    if (!clients[target]) {
-      socket.emit('chat-error', status_codes.TARGET_NOT_FOUND);
-      return;
-    }
-
-    if (clients[target].id === socket.id) {
-      socket.emit('chat-error', status_codes.TARGET_SELF);
-      return;
-    }
-
-    if (pipeline.find((p) => p.includes(socket))) {
+  socket.on('start-chat', (data, cb) => {
+    if (pipeline.find(pipe => pipe.sockets.includes(socket))) {
       socket.emit('chat-error', status_codes.ALREADY_IN_PIPELINE);
-      return;
+      return
     }
 
-    if (pipeline.find((p) => p.includes(clients[target]))) {
-      socket.emit('chat-error', status_codes.TARGET_ALREADY_IN_PIPELINE);
-      return;
+    let pipelineId: string = Math.random().toString(36).substring(2, 15);
+    while (pipeline.find(pipe => pipe.id === pipelineId)) {
+      pipelineId = Math.random().toString(36).substring(2, 15);
     }
 
-    pipeline.push([socket, clients[target]]);
+    pipeline.push({
+      id: pipelineId,
+      sockets: [socket]
+    });
 
-    console.log(`${socket.id} started chat with ${clients[target].id}`);
+    console.log('New pipe: ' + pipelineId);
+
+    cb(pipelineId);
   });
 
   socket.on('chat-message', (data) => {
     const message = data.toString();
-    const pipe = pipeline.find((p) => p.includes(socket));
+    const pipe = pipeline.find(pipe => pipe.sockets.includes(socket));
 
     if (!pipe) {
       socket.emit('chat-error', status_codes.NOT_IN_PIPELINE);
       return;
     }
 
-    pipe.forEach((client) => {
+    pipe.sockets.forEach((client) => {
       if (client.id !== socket.id) {
         client.emit('chat-message', message);
       }
     });
 
-    console.log(message);
+    console.log(`[${pipe.id}] ${message}`);
   });
 
   socket.on('end-chat', () => {
-    const pipe = pipeline.find((p) => p.includes(socket));
+    const pipe = pipeline.find(pipe => pipe.sockets.includes(socket));
 
     if (!pipe) {
       socket.emit('chat-error', status_codes.NOT_IN_PIPELINE);
       return;
     }
 
-    pipe.splice(pipe.indexOf(socket), 1);
+    pipe.sockets.forEach((client) => {
+      if (client.id !== socket.id) {
+        client.emit('chat-error', status_codes.PIPELINE_ENDED);
+      }
+    });
 
-    console.log(`${socket.id} ended chat with ${pipe[1].id}`);
+    pipeline.splice(pipeline.indexOf(pipe), 1);
+
+    console.log(`${pipe.id} was deleted`);
   });
 
   function removeSocketFromClients(socket: io.Socket) {
