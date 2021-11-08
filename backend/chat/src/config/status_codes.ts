4d65f791c67f9a111bbd2a4568133f20621f119e
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-11-08 20:32:13 +0100

    join and kick

--- backend/chat/src/config/status_codes.ts
+++ backend/chat/src/config/status_codes.ts
@@ -7,8 +7,13 @@ export default new class status_codes {
   USERNAME_IN_USE = [ 2, 'Username is already in use' ];
   TARGET_NOT_FOUND = [ 3, 'Target user could not be found' ];
   TARGET_SELF = [ 4, 'You cannot send messages to yourself' ];
-  ALREADY_IN_PIPELINE = [ 5, 'You are already in a chat room' ];
-  TARGET_ALREADY_IN_PIPELINE = [ 6, 'Target user is already in a chat room' ];
-  NOT_IN_PIPELINE = [ 7, 'You are not in a chat room' ];
-  PIPELINE_ENDED = [ 8, 'Chat room has ended' ];
+  ALREADY_IN_PIPE= [ 5, 'You are already in a chat room' ];
+  TARGET_ALREADY_IN_PIPE = [ 6, 'Target user is already in a chat room' ];
+  NOT_IN_PIPE = [ 7, 'You are not in a chat room' ];
+  PIPE_ENDED = [ 8, 'Chat room has ended' ];
+  NO_USERNAME = [ 9, 'No username provided' ];
+  PIPE_NOT_FOUND = [ 10, 'Chat room could not be found' ];
+  TARGET_NOT_IN_PIPE = [ 11, 'Target user is not in the chat room' ];
+  REMOVED_FROM_PIPE = [ 12, 'You have been removed from the chat room' ];
+  TARGET_REMOVED = (name: string) =>  [ 13, name + ' has been removed from the chat room' ];
 }
