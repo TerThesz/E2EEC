a878c18bb467fb23ed5bd1bcd1044be25a5a8a7e
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-11-13 17:51:12 +0100

    faster uuid generator

--- backend/chat/src/server.ts
+++ backend/chat/src/server.ts
@@ -2,9 +2,10 @@ import * as io from 'socket.io';
 import { User } from '@classes';
 import { COOLDOWNS, USER } from '@config/types';
 import { EventRegistry } from 'server/registries';
-import { Socket } from 'socket.io';
+import { getHostIp } from 'server/utils';
 
 const server = new io.Server();
+const host_ip = getHostIp();
 
 const cooldown_threshold = process.env.COOLDOWN_THRESHOLD || 500;
 const max_events_over_threshold = process.env.MAX_EVENTS_OVER_THRESHOLD || 10;
@@ -13,7 +14,7 @@ const threshold_clears_after = process.env.THRESHOLD_CLEARS_AFTER || 10 * 1000;
 
 const cooldown_information: COOLDOWNS = {};
 
-EventRegistry.initializeEvents();
+EventRegistry.initializeEvents(); 
 
 server.on('connection', (socket) => {
   // Handle connection
@@ -41,5 +42,6 @@ export default {
   max_events_over_threshold,
   cooldown,
   threshold_clears_after,
-  cooldown_information
+  cooldown_information,
+  host_ip
 }
--- backend/chat/src/server/utils/index.ts
+++ backend/chat/src/server/utils/index.ts
@@ -1,11 +1,13 @@
-import generateUUID from './generate_guid';
+import generateUUID from './generate_uuid';
 import preEventError from './pre_event_error';
 import eventError from './event_error';
 import callbackTimeout from './callback_timeout';
+import getHostIp from './get_host_ip';
 
 export {
   generateUUID,
   preEventError,
   eventError,
   callbackTimeout,
+  getHostIp
 }
